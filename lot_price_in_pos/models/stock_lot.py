7689404e2a9d54f028357459557868709ac682fb
hst-mohammadamaierh0 <mo.amaireh@hst.jo>
2025-12-03 14:20:59 +0300

    Keep only hr_social_security_report module

--- lot_price_in_pos/models/stock_lot.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class StockLot(models.Model):
-    _inherit = "stock.lot"
-
-    lot_price = fields.Float(string="Lot Price", help="Custom price for this lot/serial")
